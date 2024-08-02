dd6df51406c60e6b99c825009a0615a9a7350d6f
dhb-9 <dhb9@njit.edu>
2024-08-01 20:57:19 -0400

    add fields for profiles in user_model.py

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -28,8 +28,7 @@ class User(Base):
         email_verified (bool): Flag indicating if the email has been verified.
         hashed_password (str): Hashed password for security, required.
         first_name (str): Optional first name of the user.
-        last_name (str): Optional first name of the user.
-
+        last_name (str): Optional last name of the user.
         bio (str): Optional biographical information.
         profile_picture_url (str): Optional URL to a profile picture.
         linkedin_profile_url (str): Optional LinkedIn profile URL.
@@ -73,7 +72,6 @@ class User(Base):
     verification_token = Column(String, nullable=True)
     email_verified: Mapped[bool] = Column(Boolean, default=False, nullable=False)
     hashed_password: Mapped[str] = Column(String(255), nullable=False)
-
 
     def __repr__(self) -> str:
         """Provides a readable representation of a user object."""
